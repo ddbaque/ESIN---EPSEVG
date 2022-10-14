c973029dc00f40b0be735e44d0c25674bfd2f2ca
tupapiloko <baquedavid0@gmail.com>
2022-10-14 15:54:26 +0200

    n-fills

---
+++ X49037/Abin.hpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+typedef unsigned int nat;
+
+template <typename T>
+class Abin
+{
+public:
+    Abin() : _arrel(NULL){};
+    // Pre: cert
+    // Post: el resultat és un arbre sense cap element
+    Abin(Abin<T> &ae, const T &x, Abin<T> &ad);
+    // Pre: cert
+    // Post: el resultat és un arbre amb un element i dos subarbres
+
+    // Les tres grans
+    Abin(const Abin<T> &a);
+    ~Abin();
+    Abin<T> &operator=(const Abin<T> &a);
+
+    // Escriu una línia amb els elements del nivell i-èssim, d’esquerra
+    // a dreta. Cada element ha de sortir precedit d’un espai.
+    void nivell(nat i) const;
+
+private:
+    struct node
+    {
+        node *f_esq;
+        node *f_dret;
+        T info;
+    };
+    node *_arrel;
+    static node *copia_nodes(node *m);
+    static void esborra_nodes(node *m);
+    void re_nivell(node *n, nat i) const{
+        if(i == 0 or n == nullptr){
+            if(n != nullptr) cout << n->info << " ";
+            return;
+        }
+        --i;
+        re_nivell(n->f_esq, i);
+        re_nivell(n->f_dret, i);
+    }
+
+    // Aquí va l’especificació dels mètodes privats addicionals
+};
+
+// Aquí va la implementació del mètode nivell
+
+template <typename T>
+void Abin<T>::nivell(nat i) const{
+    nat j = i;
+    re_nivell(_arrel, j);
+}
---
+++ X49037/prueba.cpp
@@ -0,0 +1,8 @@
+#include "Abin.hpp"
+#include "arbreBin.hpp"
+
+int main(){
+    arbreBin<int> bt;
+    cin >> bt;
+    cout << bt << endl;
+}
