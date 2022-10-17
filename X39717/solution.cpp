018913559c1b4cf1f0043078dc6a24e815dd4eac
tupapiloko <baquedavid0@gmail.com>
2022-10-17 14:39:35 +0200

    arbre bin enfilat

--- X39717/solution.cpp
+++ X39717/solution.cpp
@@ -1,9 +1,34 @@
 #include "abin.hpp"
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
-template <typename T> 
-list<T> Abin<T>::preordre() const{
-    list<int>
+template <typename T>
+list<T> Abin<T>::preordre() const
+{
+    list<T> lst;
+    node *paux = _arrel;
+    while (paux != nullptr)
+    {
+        lst.push_back(paux->info);
+
+        if (!paux->thread_esq)
+        {
+            paux = paux->f_esq;
+        }
+        else if (!paux->thread_dret)
+        {
+            paux = paux->f_dret;
+        }
+        else
+        {
+            while (paux->thread_dret)
+            {
+                paux = paux->f_dret;
+                if(paux == nullptr) break;
+            }
+            if(paux != nullptr) paux = paux->f_dret;
+        }
+    }
+    return lst;
 }
